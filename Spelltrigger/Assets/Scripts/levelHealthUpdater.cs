46c7c671f8a48c94d7cf40b0a10f5fd1b11d1512
Tom White <whitethomas136@gmail.com>
2020-01-07 13:03:22 +0000

    UI not working

---
+++ Spelltrigger/Assets/Scripts/levelHealthUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class levelHealthUpdater : MonoBehaviour
+{
+    public Player player;
+    public Text health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<Text>().text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = ToString(player.health).text;
+    }
+}
