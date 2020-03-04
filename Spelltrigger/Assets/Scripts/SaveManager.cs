e571d2e984c98658121fb9f4942c9e10b0334f6e
Tom White <whitethomas136@gmail.com>
2020-03-04 12:43:16 +0000

    commented savemanager

--- Spelltrigger/Assets/Scripts/SaveManager.cs
+++ Spelltrigger/Assets/Scripts/SaveManager.cs
@@ -6,9 +6,14 @@ public static class SaveManager
 {
     public static void SaveSaveData(SaveData saveData)
     {
+        // This function saves the data to a file, by converting the serialisable savedata class
+        // to a binary stream and saving that as a file
         BinaryFormatter formatter = new BinaryFormatter();
+        // Generates the file path, by finding a basic path to save data relevant to the OS
+        // (on windows this will be \AppData\LocalLow\DefaultCompany\Spelltrigger)
         string path = Application.persistentDataPath + "/savedata.data";
 
+        // Opens the file and saves the serialised class, then closes the file
         using (FileStream stream = new FileStream(path, FileMode.Create))
         {
             formatter.Serialize(stream, saveData);
@@ -18,10 +23,16 @@ public static class SaveManager
 
     public static SaveData LoadSaveData()
     {
+        // This function loads the data from a file, by converting the file back into
+        // the serializable savedata class and returning it
         BinaryFormatter formatter = new BinaryFormatter();
+        // Generates the file path, by finding a basic path to save data relevant to the OS
+        // (on windows this will be \AppData\LocalLow\DefaultCompany\Spelltrigger)
         string path = Application.persistentDataPath + "/savedata.data";
         if (File.Exists(path))
         {
+            // Opens the file and deserialises it to get the class, then returns that class
+            // Only runs if the file exists
             using (FileStream stream = new FileStream(path, FileMode.Open))
             {
                 SaveData data = formatter.Deserialize(stream) as SaveData;
@@ -29,6 +40,7 @@ public static class SaveManager
                 return data;
             }
         }
+        // If a file is not found, it will create a new savedata class
         else
         {
             return new SaveData();
